cec68f97c433bc2ccf8a3168e11b812bba42cdbd
Martin Tichovský <35766028+MartinTichovsky@users.noreply.github.com>
2025-01-29 18:53:11 +0100

    feat: add more callline commands (#17)
    Co-authored-by: Martin Tichovsky <martin.tichovsky@infor.com>

--- packages/interceptor/test.unit.commands.ts
+++ packages/interceptor/test.unit.commands.ts
@@ -12,13 +12,17 @@ declare global {
              */
             callLine(): Chainable<CallLine>;
             /**
+             * Clean the CallLine array and start storing the values from the beginning
+             */
+            callLineClean(): void;
+            /**
              * The last existing entry. It can be `undefined` if there is no entry at
              * the moment or `next` has not been called. Otherwise it always returns
              * the last entry invoked by `next`.
              */
             callLineCurrent(): Chainable<unknown | unknown[] | undefined>;
             /**
-             * The number of all entries
+             * Get the number of all entries.
              */
             callLineLength(): Chainable<number>;
             /**
@@ -30,6 +34,10 @@ declare global {
              * `["something", 1, true]`.
              */
             callLineNext(): Chainable<unknown | unknown[] | undefined>;
+            /**
+             * Resets the counter and starts from the first entry on the next call to `cy.callLineNext`
+             */
+            callLineReset(): void;
         }
     }
 }
@@ -38,7 +46,19 @@ declare global {
     const callLine = getCallLine();
 
     Cypress.Commands.add("callLine", () => cy.wrap(callLine));
+    Cypress.Commands.add("callLineClean", () => callLine.clean());
     Cypress.Commands.add("callLineCurrent", () => cy.wrap(callLine.current));
-    Cypress.Commands.add("callLineLength", () => cy.wrap(callLine.length));
-    Cypress.Commands.add("callLineNext", () => cy.wrap(callLine.next));
+    Cypress.Commands.addQuery("callLineLength", () => () => callLine.length);
+    Cypress.Commands.addQuery("callLineNext", () => {
+        let next: unknown;
+
+        return () => {
+            if (next === undefined) {
+                next = callLine.next;
+            }
+
+            return next;
+        };
+    });
+    Cypress.Commands.add("callLineReset", () => callLine.reset());
 })();
